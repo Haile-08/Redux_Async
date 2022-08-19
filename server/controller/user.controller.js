b5622b5cd209ede9bc227311230fd928e402ccfb
Haile-08 <haile4cmd@gmail.com>
2022-08-19 09:50:26 +0300

    created the webtoken in signup

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,5 +1,10 @@
 const user = require("../model/User.Schema");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
 //user login
 const loginUser = async (req, res) => {
   res.json({ login: "loged" });
@@ -11,7 +16,8 @@ const signUpUser = async (req, res) => {
 
   try {
     const User = await user.signUp(email, password);
-    res.status(200).json({ email, User });
+    const token = createToken(User._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
